96ce71d05c31e4a5f16f320f0027a0bf5ad4f1ea
lofeodo <daniel.lofeodo@gmail.com>
2025-04-23 14:48:41 -0400

    q1 vae code and loss function in train_vae

--- q1_vae.py
+++ q1_vae.py
@@ -23,8 +23,8 @@ def log_likelihood_bernoulli(mu, target):
     mu = mu.view(batch_size, -1)
     target = target.view(batch_size, -1)
 
-    #TODO: compute log_likelihood_bernoulli
-    raise NotImplementedError
+    ll_bernoulli = target * torch.log(mu) + (1 - target) * torch.log(1 - mu)
+    ll_bernoulli = torch.sum(ll_bernoulli, dim=1)
 
     return ll_bernoulli
 
@@ -46,8 +46,8 @@ def log_likelihood_normal(mu, logvar, z):
     logvar = logvar.view(batch_size, -1)
     z = z.view(batch_size, -1)
 
-    #TODO: compute log normal
-    raise NotImplementedError
+    log_likelihood = -0.5 * torch.tensor(2 * math.pi, device=mu.device).log() - logvar / 2 - (z - mu) ** 2 / (2 * torch.exp(logvar))
+    ll_normal = torch.sum(log_likelihood, dim=1)
     
     return ll_normal
 
@@ -66,7 +66,8 @@ def log_mean_exp(y):
     sample_size = y.size(1)
 
     #TODO: compute log_mean_exp
-    raise NotImplementedError
+    a, _ = torch.max(y, dim=1, keepdim=True)    
+    lme = torch.log(torch.mean(torch.exp(y - a), dim=1)) + a.squeeze(1)
 
     return lme 
 
@@ -91,7 +92,11 @@ def kl_gaussian_gaussian_analytic(mu_q, logvar_q, mu_p, logvar_p):
     logvar_p = logvar_p.view(batch_size, -1)
 
     #TODO: compute kld
-    raise NotImplementedError
+    var_q = torch.exp(logvar_q)
+    var_p = torch.exp(logvar_p)
+    term1 = logvar_p - logvar_q
+    term2 = (var_q + (mu_q - mu_p)**2) / var_p
+    kl_gg = 0.5 * torch.sum(term1 + term2 - 1, dim=1)
 
     return kl_gg
 
@@ -118,6 +123,21 @@ def kl_gaussian_gaussian_mc(mu_q, logvar_q, mu_p, logvar_p, num_samples=1):
     logvar_p = logvar_p.view(batch_size, -1).unsqueeze(1).expand(batch_size, num_samples, input_size)
 
     #TODO: compute kld
-    raise NotImplementedError
+    std_q = torch.exp(0.5 * logvar_q)
+    eps = torch.randn_like(std_q)
+    z = mu_q + eps * std_q 
+
+    log_2pi = torch.log(torch.tensor(2 * math.pi, device=mu_q.device))
+
+    # Compute log q(z)
+    log_qz = -0.5 * (logvar_q + log_2pi + ((z - mu_q) ** 2) / torch.exp(logvar_q))
+    log_qz = log_qz.sum(dim=2)
+
+    # Compute log p(z)
+    log_pz = -0.5 * (logvar_p + log_2pi + ((z - mu_p) ** 2) / torch.exp(logvar_p))
+    log_pz = log_pz.sum(dim=2)
+
+    # Monte Carlo estimate of KL divergence
+    kl_mc = (log_qz - log_pz).mean(dim=1)
 
     return kl_mc
